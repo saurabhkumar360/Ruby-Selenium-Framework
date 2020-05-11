11f2d44de88c6b0e7b2d3f8f9d612bd467071541
Prashant Keshari <prashant.k@360logica.com>
2020-05-11 11:53:01 +0530

    correct the path of driver

--- RubySelenium/features/support/env.rb
+++ RubySelenium/features/support/env.rb
@@ -1,7 +1,7 @@
 require 'selenium-webdriver'
 require 'cucumber'
 Before do
-  chromedriver_path = File.join(File.absolute_path('.', File.dirname(__FILE__)),"../drivers","chromedriver.exe")
+  chromedriver_path = File.join(File.absolute_path('.', File.dirname(__FILE__)),"/../../drivers","chromedriver.exe")
   Selenium::WebDriver::Chrome.driver_path = chromedriver_path
   $driver = Selenium::WebDriver.for :chrome
   $driver.get('http://opencart.abstracta.us/')
